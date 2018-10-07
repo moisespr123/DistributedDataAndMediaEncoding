2b975a5cbaf53312ad1426147a8c049179398081
moisesmcardona <moises@moises-studios.com>
2018-10-06 20:42:41 -0400

    Added retrieve_files C# project to retrieve result files from submitted tasks.

---
+++ retrieve_files/retrieve_files/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace retrieve_files
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.userKey = new System.Windows.Forms.TextBox();
+            this.showFilesButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.downloadAll = new System.Windows.Forms.Button();
+            this.categoriesListBox = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.downloadSelected = new System.Windows.Forms.Button();
+            this.filesListBox = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Your Account Weak Key:";
+            // 
+            // userKey
+            // 
+            this.userKey.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.userKey.Location = new System.Drawing.Point(15, 25);
+            this.userKey.Name = "userKey";
+            this.userKey.Size = new System.Drawing.Size(632, 20);
+            this.userKey.TabIndex = 1;
+            // 
+            // showFilesButton
+            // 
+            this.showFilesButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.showFilesButton.Location = new System.Drawing.Point(653, 23);
+            this.showFilesButton.Name = "showFilesButton";
+            this.showFilesButton.Size = new System.Drawing.Size(75, 23);
+            this.showFilesButton.TabIndex = 2;
+            this.showFilesButton.Text = "Show Files";
+            this.showFilesButton.UseVisualStyleBackColor = true;
+            this.showFilesButton.Click += new System.EventHandler(this.showFilesButton_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 67);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(716, 538);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.downloadAll);
+            this.panel1.Controls.Add(this.categoriesListBox);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(352, 532);
+            this.panel1.TabIndex = 0;
+            // 
+            // downloadAll
+            // 
+            this.downloadAll.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.downloadAll.Location = new System.Drawing.Point(6, 495);
+            this.downloadAll.Name = "downloadAll";
+            this.downloadAll.Size = new System.Drawing.Size(343, 23);
+            this.downloadAll.TabIndex = 2;
+            this.downloadAll.Text = "Download all files in selected category";
+            this.downloadAll.UseVisualStyleBackColor = true;
+            this.downloadAll.Click += new System.EventHandler(this.downloadAll_Click);
+            // 
+            // categoriesListBox
+            // 
+            this.categoriesListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.categoriesListBox.FormattingEnabled = true;
+            this.categoriesListBox.Location = new System.Drawing.Point(6, 16);
+            this.categoriesListBox.Name = "categoriesListBox";
+            this.categoriesListBox.Size = new System.Drawing.Size(343, 472);
+            this.categoriesListBox.TabIndex = 1;
+            this.categoriesListBox.SelectedIndexChanged += new System.EventHandler(this.categoriesListBox_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Categories";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.downloadSelected);
+            this.panel2.Controls.Add(this.filesListBox);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(361, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(352, 532);
+            this.panel2.TabIndex = 1;
+            // 
+            // downloadSelected
+            // 
+            this.downloadSelected.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.downloadSelected.Location = new System.Drawing.Point(3, 495);
+            this.downloadSelected.Name = "downloadSelected";
+            this.downloadSelected.Size = new System.Drawing.Size(343, 23);
+            this.downloadSelected.TabIndex = 5;
+            this.downloadSelected.Text = "Download Selected File(s)";
+            this.downloadSelected.UseVisualStyleBackColor = true;
+            this.downloadSelected.Click += new System.EventHandler(this.downloadSelected_Click);
+            // 
+            // filesListBox
+            // 
+            this.filesListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.filesListBox.FormattingEnabled = true;
+            this.filesListBox.Location = new System.Drawing.Point(3, 16);
+            this.filesListBox.Name = "filesListBox";
+            this.filesListBox.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.filesListBox.Size = new System.Drawing.Size(343, 472);
+            this.filesListBox.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Files";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(740, 617);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.showFilesButton);
+            this.Controls.Add(this.userKey);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Distributed Data and Media Processing | Data Retriever";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox userKey;
+        private System.Windows.Forms.Button showFilesButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button downloadAll;
+        private System.Windows.Forms.ListBox categoriesListBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button downloadSelected;
+        private System.Windows.Forms.ListBox filesListBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ retrieve_files/retrieve_files/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace retrieve_files
+{
+
+    public partial class Form1 : Form
+    {
+        private dynamic items;
+        private List<string> categories = new List<string> { };
+        private List<string> files = new List<string> { };
+        private List<string> keys = new List<string> { };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private static string GetFiles(string key)
+        {
+            using (var client = new HttpClient())
+            using (var formData = new MultipartFormDataContent())
+            {
+                {
+                    formData.Add(new StringContent(key), "k");
+                    Uri uri = new Uri("http://boinc.moisescardona.me/data_retriever_get_files.php");
+                    HttpResponseMessage response = client.PostAsync(uri, formData).Result;
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine("Error");
+                    }
+                    StreamReader reader = new StreamReader(response.Content.ReadAsStreamAsync().Result);
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+        private static async Task<bool> DownloadFile(string key, string filekey, string path)
+        {
+            using (var client = new HttpClient())
+            using (var formData = new MultipartFormDataContent())
+            {
+                {
+                    formData.Add(new StringContent(key), "k");
+                    formData.Add(new StringContent(filekey), "f");
+                    Uri uri = new Uri("http://boinc.moisescardona.me/download_file_post.php");
+                    HttpResponseMessage response = client.PostAsync(uri, formData).Result;
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        return false;
+                    }
+                    FileStream fileStream = new FileStream(path, FileMode.Create);
+                    await response.Content.CopyToAsync(fileStream);
+                    fileStream.Close();
+                    return true;
+                }
+            }
+        }
+        private void updateListBoxes()
+        {
+            string reply = GetFiles(userKey.Text);
+            items = Newtonsoft.Json.JsonConvert.DeserializeObject(reply);
+            categories.Clear();
+            categories.Add("None");
+            foreach (dynamic item in items)
+            {
+                string category = item["category"];
+                if (category != "None")
+                    if (!categories.Any(category.Contains))
+                        categories.Add(category);
+            }
+            categoriesListBox.DataSource = null;
+            categoriesListBox.DataSource = categories;
+        }
+        private void showFilesButton_Click(object sender, EventArgs e)
+        {
+            updateListBoxes();
+        }
+
+        private void populateFilesListBox(string category)
+        {
+            files.Clear();
+            keys.Clear();
+            foreach (dynamic item in items)
+            {
+                if (item["category"] == category)
+                {
+                    files.Add(item["name"].ToString());
+                    keys.Add(item["key"].ToString());
+                }
+            }
+            filesListBox.DataSource = null;
+            filesListBox.DataSource = files;
+        }
+        private void categoriesListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (categoriesListBox.SelectedIndex >= 0)
+            {
+                populateFilesListBox(categoriesListBox.SelectedItem.ToString());
+            }
+        }
+
+        private async void downloadAll_Click(object sender, EventArgs e)
+        {
+            if (categoriesListBox.SelectedIndex >= 0)
+            {
+                folderBrowserDialog1.ShowNewFolderButton = true;
+                folderBrowserDialog1.Description = "Browse for a folder to save the files from the selected category.";
+                DialogResult result = folderBrowserDialog1.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    string FolderPath = folderBrowserDialog1.SelectedPath;
+                    if (categoriesListBox.SelectedItem.ToString() != "None")
+                    {
+                        FolderPath += "\\" + categoriesListBox.SelectedItem.ToString().Replace('\\', '_').Replace('/', '_').Replace(':', '_').Replace('*', '_').Replace('\"', '_').Replace('?', '_').Replace('<', '_').Replace('>', '_').Replace('|', '_');
+                    }
+                    Directory.CreateDirectory(FolderPath);
+                    for (int i = 0; i < files.Count; i++)
+                    {
+                        using (WebClient client = new WebClient())
+                        {
+                            await DownloadFile(userKey.Text, keys[i], FolderPath + "\\" + files[i]);
+                        }
+                    }
+                }
+            }
+            MessageBox.Show("Downloads completed!");
+            updateListBoxes();
+        }
+
+        private async void downloadSelected_Click(object sender, EventArgs e)
+        {
+            if (categoriesListBox.SelectedIndex >= 0)
+            {
+                folderBrowserDialog1.ShowNewFolderButton = true;
+                folderBrowserDialog1.Description = "Browse for a folder to save the files from the selected category.";
+                DialogResult result = folderBrowserDialog1.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    string FolderPath = folderBrowserDialog1.SelectedPath;
+                    if (categoriesListBox.SelectedItem.ToString() != "None")
+                    {
+                        FolderPath += "\\" + categoriesListBox.SelectedItem.ToString().Replace('\\', '_').Replace('/', '_').Replace(':', '_').Replace('*', '_').Replace('\"', '_').Replace('?', '_').Replace('<', '_').Replace('>', '_').Replace('|', '_');
+                    }
+                    Directory.CreateDirectory(FolderPath);
+                    foreach (int index in filesListBox.SelectedIndices)
+                    {
+                        using (WebClient client = new WebClient())
+                        {
+                            await DownloadFile(userKey.Text, keys[index], FolderPath + "\\" + files[index]);
+                        }
+                    }
+                }
+            }
+            MessageBox.Show("Downloads completed!");
+            updateListBoxes();
+        }
+    }
+}
