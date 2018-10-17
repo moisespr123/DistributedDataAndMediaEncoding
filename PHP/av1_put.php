1e4b9e83672df0eddc7d4f1bb0ffe12edf6c552c
moisesmcardona <moises@moises-studios.com>
2018-10-17 09:51:01 -0400

    use file name for wu name and moves file instead of deleting it to RAW files folder

--- PHP/av1_put.php
+++ PHP/av1_put.php
@@ -19,19 +19,18 @@ if (isset($_POST['k'])) {
     if ($user_id == 1) {
         chdir($templates_folder);
         $filename = $_POST['f'];
-        $random_token = bin2hex(random_bytes(16));
-        $wu_template = fopen($random_token . "_wu", "w");
-        $result_template = fopen($random_token . "_result", "w");
+        $wu_template = fopen($filename . "_wu", "w");
+        $result_template = fopen($filename . "_result", "w");
         $app = "rav1e_encoder";
         fwrite($wu_template, generate_wu_general_template_cmd($filename . ".y4m", $_POST['c']));
         fwrite($result_template, generate_put_result_template($filename . ".ivf"));
-        $job_creation_command = return_job_string($app, $random_token, $filename . ".y4m");
+        $job_creation_command = return_job_string($app, $filename, $filename . ".y4m");
         fclose($wu_template);
         fclose($result_template);
         chdir($root_folder);
         exec($job_creation_command);
         insertY4mFile($mysqli, $user_id, $_POST['n'] , $filename, $app);
-        unlink($download_folder . $filename . ".y4m");
+        rename($download_folder .  $filename . ".y4m", $move_folder .  $filename . ".y4m");
         echo "Done";
     } else {
         echo "Incorrect User Key";
